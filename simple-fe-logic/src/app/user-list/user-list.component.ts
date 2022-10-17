82d5a45a5d8cbe8dee7b530444431310694377cb
Patrik Kolenovsky <patrik.kolenovsky@tietoevry.com>
2022-10-17 20:09:26 +0200

    - add formvalidation

--- simple-fe-logic/src/app/user-list/user-list.component.ts
+++ simple-fe-logic/src/app/user-list/user-list.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from "../user";
 import {UserService} from "../user.service";
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {Role} from "../role";
-import {Product} from "../product";
 
 @Component({
   selector: 'app-user-list',
@@ -12,33 +11,43 @@ import {Product} from "../product";
 })
 export class UserListComponent implements OnInit {
 
+  phoneValidatorPattern = new RegExp(/\+420\s\d{3}\s\d{3}\s\d{3}/);
   users: User[] = [];
-  addUserForm: FormGroup;
-  constructor(private userService: UserService, private formBuilder: FormBuilder) {
-    this.addUserForm = this.formBuilder.group({
-      userId: 0,
-      userName: "",
-      email: "",
-      birthDate: "",
-      phoneNumber: "",
-      role: Role.USER
-    });
+  addUserForm: FormGroup = new FormGroup({
+    userId: new FormControl(0, Validators.required),
+    userName: new FormControl("", Validators.required),
+    email: new FormControl("", [Validators.required, Validators.email]),
+    birthDate: new FormControl("", Validators.required),
+    phoneNumber: new FormControl("", [Validators.required, Validators.pattern(this.phoneValidatorPattern)]),
+    role: new FormControl(Role.USER, Validators.required)
+  });
+
+  constructor(private userService: UserService) {
   }
 
-  addUser() {
-    var userId = this.addUserForm.value.userId;
-    var userName = this.addUserForm.value.userName;
-    var email = this.addUserForm.value.email;
-    var birthDate = this.addUserForm.value.birthDate;
-    var phoneNumber = this.addUserForm.value.phoneNumber;
-    var role = this.addUserForm.value.role;
 
-    let user = new User(userId, userName, email, birthDate, phoneNumber, role);
-    this.userService.addUser(user);
-    this.users = this.userService.getUsers();
+  addUser() {
+    // if (this.addUserForm.errors!) {
+      var userId = this.addUserForm.value.userId;
+      var userName = this.addUserForm.value.userName;
+      var email = this.addUserForm.value.email;
+      var birthDate = this.addUserForm.value.birthDate;
+      var phoneNumber = this.addUserForm.value.phoneNumber;
+      var role = this.addUserForm.value.role;
+
+      let user = new User(userId, userName, email, birthDate, phoneNumber, role);
+      this.userService.addUser(user);
+      this.users = this.userService.getUsers();
+    // } else {
+    //   window.alert("Bad form format" + this.addUserForm.errors);
+    // }
   }
 
   ngOnInit(): void {
+  }
+
+  get phoneNumber () {
+    return this.addUserForm.get('phoneNumber');
   }
 
 }
