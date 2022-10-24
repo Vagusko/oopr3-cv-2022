b7cfe2a851a1cf87093518001b866f1a0ffa962b
Patrik Kolenovsky <patrik.kolenovsky@tietoevry.com>
2022-10-24 20:24:07 +0200

    - add material ui + form

--- simple-fe-logic/src/app/app.module.ts
+++ simple-fe-logic/src/app/app.module.ts
@@ -5,6 +5,12 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import { UserListComponent } from './user-list/user-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+import {MatCardModule} from "@angular/material/card";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 @NgModule({
   declarations: [
@@ -12,10 +18,16 @@ import { UserListComponent } from './user-list/user-list.component';
     ProductListComponent,
     UserListComponent
   ],
-    imports: [
-        BrowserModule,
-        ReactiveFormsModule
-    ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCardModule,
+    MatToolbarModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
