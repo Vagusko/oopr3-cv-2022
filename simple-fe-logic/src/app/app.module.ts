a55c95c9a66b10004001cb09f1644dcd8c6916f4
Patrik Kolenovsky <patrik.kolenovsky@tietoevry.com>
2022-10-24 20:30:27 +0200

    - add basic menu

--- simple-fe-logic/src/app/app.module.ts
+++ simple-fe-logic/src/app/app.module.ts
@@ -11,6 +11,8 @@ import {MatInputModule} from "@angular/material/input";
 import {MatSelectModule} from "@angular/material/select";
 import {MatCardModule} from "@angular/material/card";
 import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
@@ -26,7 +28,9 @@ import {MatToolbarModule} from "@angular/material/toolbar";
     MatInputModule,
     MatSelectModule,
     MatCardModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
