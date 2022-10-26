53260a6acbdc923f3e4e9e8ea4f4008310226a06
Patrik Kolenovsky <patrik.kolenovsky@tietoevry.com>
2022-10-26 14:51:47 +0200

    - Add angular material

--- simple-fe-logic/src/app/app.module.ts
+++ simple-fe-logic/src/app/app.module.ts
@@ -5,6 +5,13 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import { UserListComponent } from './user-list/user-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
@@ -12,10 +19,17 @@ import { UserListComponent } from './user-list/user-list.component';
     ProductListComponent,
     UserListComponent
   ],
-    imports: [
-        BrowserModule,
-        ReactiveFormsModule
-    ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
