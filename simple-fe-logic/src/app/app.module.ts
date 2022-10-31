3054380db0fa73f5e919f575071562031a1b890b
Patrik Kolenovsky <patrik.kolenovsky@tietoevry.com>
2022-10-31 12:01:01 +0100

    - split to feature hiearchy

--- simple-fe-logic/src/app/app.module.ts
+++ simple-fe-logic/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { AppComponent } from './core/app.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
 import {ReactiveFormsModule} from "@angular/forms";
-import { UserListComponent } from './user-list/user-list.component';
+import { UserListComponent } from './user/user-list/user-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
